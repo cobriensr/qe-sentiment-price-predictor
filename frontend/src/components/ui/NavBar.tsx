da3093939d5ed941515f435be65056eb4b6a4d76
Charles OBrien Sr <charles.a.obrien@outlook.com>
2025-06-16 17:42:11 -0500

    updated navbar caps

--- frontend/src/components/ui/NavBar.tsx
+++ frontend/src/components/ui/NavBar.tsx
@@ -4,7 +4,7 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
-export default function Navbar() {
+export default function NavBar() {
   const pathname = usePathname()
 
   const navItems = [
